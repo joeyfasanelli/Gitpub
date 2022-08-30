5ce5aa5f586350fa4a0307b9737f382bbb746f01
Joey Fasanelli <joeyfas1704@gmail.com>
2022-08-30 13:13:43 -0400

    Rendered individual drink in the show view

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.get("/drinks", (req, res)=>{
 });
 
 app.get("/drinks/:id", (req, res)=>{
-    res.send(req.params.id)
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.id],
+    });
 });
 
